957baa1bccf96619caaac9cf35e28dd58966c972
RPGP1 <RPGP1@users.noreply.github.com>
2013-11-18 22:48:57 +0900

    Update sortposts.rb

--- _plugins/sortposts.rb
+++ _plugins/sortposts.rb
@@ -1,4 +1,25 @@
 # coding: utf-8
 
 module Jekyll
+  class SortPostsTag < Liquid::Tag
+    def initialize(tag_name, markup, tokens)
+      super
+      @arg = markup.to_i
+    end
+    
+    def render(context)
+      site = context.registers[:site]
+      post = site["posts"]
+      post.sort!{|a,b| (-1) * (a["date"] <=> b["date"])}
+      @arg.times do |i|
+        context.stack do
+          context["onepost"] = post[i]
+          super
+        end
+      end
+      ""
+    end
+  end
 end
+
+Liquid::Template.register_tag("sortposts", Jekyll::sortPostsTag)
