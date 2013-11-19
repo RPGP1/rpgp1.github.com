db3e4b79010173283c785828e14376adb8177bf6
RPGP1 <RPGP1@users.noreply.github.com>
2013-11-19 11:21:32 +0900

    Update sortposts.rb

--- _plugins/sortposts.rb
+++ _plugins/sortposts.rb
@@ -5,7 +5,7 @@ module Jekyll
     def render(context)
       site = context.registers[:site]
       posts = site.posts
-      posts.sort!{|a,b| (-1) * (a["date"] <=> b["date"])}
+      posts.sort!{|a,b| (-1) * (a.date <=> b.date)}
       context.stack do
         context["spost"] = posts
         return super
