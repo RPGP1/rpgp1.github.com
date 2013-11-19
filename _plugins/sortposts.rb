e3e6df7a69dd26e81a84f3ec38c1d4cbb04b131d
RPGP1 <RPGP1@users.noreply.github.com>
2013-11-19 11:07:45 +0900

    Update sortposts.rb

--- _plugins/sortposts.rb
+++ _plugins/sortposts.rb
@@ -4,8 +4,8 @@ module Jekyll
   class SortPostsBlock < Liquid::Block
     def render(context)
       site = context.registers[:site]
-      posts = site["posts"]
-#      posts.sort!{|a,b| (-1) * (a["date"] <=> b["date"])}
+      posts = site.posts
+      posts.sort!{|a,b| (-1) * (a["date"] <=> b["date"])}
       context.stack do
         context["spost"] = posts
         return super
